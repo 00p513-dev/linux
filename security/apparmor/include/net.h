28c0baa28476d21b6ff0605be3d9e68df8cf4c39
John Johansen <john.johansen@canonical.com>
2022-09-07 12:46:30 -0700

    UBUNTU: SAUCE: apparmor4.0.0 [47/90]: af_unix mediation
    BugLink: http://bugs.launchpad.net/bugs/2028253
    
    af_socket mediation did not make it into 4.17 so add remaining out
    of tree patch
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    (cherry picked from https://gitlab.com/jjohansen/apparmor-kernel)
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
    (cherry picked from commit 651f0b2cc1d78cb7afbc0fcadc166ed0bcc78dd3
    https://git.launchpad.net/~apparmor-dev/ubuntu-kernel-next)
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- security/apparmor/include/net.h
+++ security/apparmor/include/net.h
@@ -49,6 +49,7 @@
 struct aa_sk_ctx {
 	struct aa_label *label;
 	struct aa_label *peer;
+	struct path path;
 };
 
 #define SK_CTX(X) ((X)->sk_security)
@@ -78,6 +79,9 @@ static inline struct aa_sk_ctx *aa_sock(const struct sock *sk)
 ({						\
 	int __e;				\
 	switch ((FAMILY)) {			\
+	case AF_UNIX:				\
+		__e = aa_unix_ ## FN;		\
+		break;				\
 	default:				\
 		__e = DEF_FN;			\
 	}					\
